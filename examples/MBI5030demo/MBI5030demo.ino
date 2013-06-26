948ce612bb96345f7563e42d9c88b3ff675f7739
Robert Spitzenpfeil <robert@linuxbox.site>
2013-06-26 20:00:14 +0200

    Updated 'ASCII' comments.

--- examples/MBI5030demo/MBI5030demo.ino
+++ examples/MBI5030demo/MBI5030demo.ino
@@ -17,68 +17,80 @@
 // Make sure to get the latest MBI5030 datasheet (at least version Jan. 2009)!
 // 
 //
-// 1.) MBI5030 with internal LEDs - Jumper in place!
+// 1) MBI5030 with internal LEDs - Jumper in place!
 //                                                
-//                            +----------------------+
-//                            |                      |
-//                    GND O---O 1  GND      VCC   24 O---O 5V
-//                            |                      |
-//    Arduino Uno pin #14 O---O 2  SDI      R-EXT 23 O
-//                            |                      |
-//    Arduino Uno pin #15 O---O 3  DCLK     SDO   22 O---O Arduino Uno pin #17 (Analog 1)
-//                            |                      |
-//    Arduino Uno pin #16 O---O 4  LE       GCLK  21 O---O Arduino Uno pin #8 
-//                            |                      |
-//                            O 5  LED0     LED15 20 O
-//                            |                      |
-//                            O 6  LED1     LED14 19 O
-//                            |                      |
-//                            O 7  LED2     LED13 18 O
-//                            |                      |
-//                            O 8  LED3     LED12 17 O
-//                            |                      |
-//                            O 9  LED4     LED11 16 O
-//                            |                      |
-//                            O 10 LED5     LED10 15 O
-//                            |                      |
-//                            O 11 LED6     LED9  14 O
-//                            |                      |
-//                            O 12 LED7     LED8  13 O
-//                            |                      |
-//                            +----------------------+
+//                                 +----------------------+
+//                                 |                      |
+//                                 |      +--------+      |
+//                                 |      |XXXXXXXX|      |
+//                                 |      +--------+      |
+//                                 |       LED-sel.       |
+//                                 |                      |
+//                                 |                      |
+//                         GND O---O 1  GND      VCC   24 O---O 5V
+//                                 |                      |
+//    Arduino Uno pin #14 (A0) O---O 2  SDI      R-EXT 23 O
+//                                 |                      |
+//    Arduino Uno pin #15 (A1) O---O 3  DCLK     SDO   22 O---O Arduino Uno pin #17 (A3)
+//                                 |                      |
+//    Arduino Uno pin #16 (A2) O---O 4  LE       GCLK  21 O---O Arduino Uno pin #8 
+//                                 |                      |
+//                                 O 5  LED0     LED15 20 O
+//                                 |                      |
+//                                 O 6  LED1     LED14 19 O
+//                                 |                      |
+//                                 O 7  LED2     LED13 18 O
+//                                 |                      |
+//                                 O 8  LED3     LED12 17 O
+//                                 |                      |
+//                                 O 9  LED4     LED11 16 O
+//                                 |                      |
+//                                 O 10 LED5     LED10 15 O
+//                                 |                      |
+//                                 O 11 LED6     LED9  14 O
+//                                 |                      |
+//                                 O 12 LED7     LED8  13 O
+//                                 |                      |
+//                                 +----------------------+
 //                   
 //                        
-// 2.) MBI5030 with external LEDs - Jumper pulled!                     
+// 2) MBI5030 with external LEDs - Jumper pulled!                     
 //                                                
-//                            +----------------------+
-//                            |                      |
-//                    GND O---O 1  GND      VCC   24 O---O 5V
-//                            |                      |
-//    Arduino Uno pin #14 O---O 2  SDI      R-EXT 23 O
-//                            |                      |
-//    Arduino Uno pin #15 O---O 3  DCLK     SDO   22 O---O Arduino Uno pin #17 (Analog 1)
-//                            |                      |
-//    Arduino Uno pin #16 O---O 4  LE       GCLK  21 O---O Arduino Uno pin #8 
-//                            |                      |
-//                   +--->|---O 5  LED0     LED15 20 O---|<---+
-//                   |        |                      |        | 
-//                   o--->|---O 6  LED1     LED14 19 O---|<---o
-//                   |        |                      |        | 
-//                   o--->|---O 7  LED2     LED13 18 O---|<---o    N:  number of LEDs per channel
-//                   |        |                      |        |    Vf: LED forward voltage 
-//                   o--->|---O 8  LED3     LED12 17 O---|<---o         
-//                   |        |                      |        |    V-LED: abs. max. 17V
-//                   o--->|---O 9  LED4     LED11 16 O---|<---o
-//                   |        |                      |        |
-//                   o--->|---O 10 LED5     LED10 15 O---|<---o
-//                   |        |                      |        |
-//                   o--->|---O 11 LED6     LED9  14 O---|<---o
-//                   |        |                      |        |
-//                   o--->|---O 12 LED7     LED8  13 O---|<---o---O V-LED ( approx. N x Vf + 0.7V )
-//                   |        |                      |        |
-//                   |        +----------------------+        |
-//                   |                                        |
-//                   +----------------------------------------+
+//                                 +----------------------+
+//                                 |                      |
+//                                 |      +--------+      |
+//                                 |      |        |      |
+//                                 |      +--------+      |
+//                                 |       LED-sel.       |
+//                                 |                      |
+//                                 |                      |
+//                         GND O---O 1  GND      VCC   24 O---O 5V
+//                                 |                      |
+//    Arduino Uno pin #14 (A0) O---O 2  SDI      R-EXT 23 O
+//                                 |                      |
+//    Arduino Uno pin #15 (A1) O---O 3  DCLK     SDO   22 O---O Arduino Uno pin #17 (A3)
+//                                 |                      |
+//    Arduino Uno pin #16 (A2) O---O 4  LE       GCLK  21 O---O Arduino Uno pin #8 
+//                                 |                      |
+//                        +--->|---O 5  LED0     LED15 20 O---|<---+
+//                        |        |                      |        | 
+//                        o--->|---O 6  LED1     LED14 19 O---|<---o
+//                        |        |                      |        | 
+//                        o--->|---O 7  LED2     LED13 18 O---|<---o    N:  number of LEDs per channel
+//                        |        |                      |        |    Vf: LED forward voltage 
+//                        o--->|---O 8  LED3     LED12 17 O---|<---o         
+//                        |        |                      |        |    V-LED: abs. max. 17V
+//                        o--->|---O 9  LED4     LED11 16 O---|<---o
+//                        |        |                      |        |
+//                        o--->|---O 10 LED5     LED10 15 O---|<---o
+//                        |        |                      |        |
+//                        o--->|---O 11 LED6     LED9  14 O---|<---o
+//                        |        |                      |        |
+//                        o--->|---O 12 LED7     LED8  13 O---|<---o---O V-LED ( approx. N x Vf + 0.7V )
+//                        |        |                      |        |
+//                        |        +----------------------+        |
+//                        |                                        |
+//                        +----------------------------------------+
 //
 
 #include <avr/io.h>
